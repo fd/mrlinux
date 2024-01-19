b449a5a0c5f2c2c64954d188a86070bbcba05519
Simon Menke <simon.menke@gmail.com>
2024-01-19 15:54:10 +0100

    Made generic over systems

--- flake.nix
+++ flake.nix
@@ -1,14 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    attic.url = "github:zhaofengli/attic";
+
     nix-ld-rs.url = "github:nix-community/nix-ld-rs";
   };
 
-  outputs = { self, nixpkgs, nix-ld-rs }:
-    (
+  outputs = { self, nixpkgs, flake-utils, devshell, attic, nix-ld-rs }:
+    (flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+            attic.overlays.default
+          ];
+        };
 
         base-os = import ./stacks/lxc/base-os.nix { inherit nixpkgs pkgs; };
 
@@ -20,16 +29,38 @@
               ${self.packages.${system}.lxc}/tarball/nixos-system-x86_64-linux.tar.xz \
               --alias mrlinux
           '';
+
+        releasePackages = pkgs.runCommandNoCC "release-packages"
+          {
+            packages = [ ]
+            ++ (builtins.attrValues (builtins.removeAttrs self.packages."x86_64-linux" [ "release-packages" ]))
+            ++ (builtins.attrValues (builtins.removeAttrs self.packages."aarch64-linux" [ "release-packages" ]));
+          }
+          ''
+            set -e
+            mkdir -p $out
+            for p in $packages; do
+              ln -s $p $out/$(basename $p)
+            done
+          '';
       in
       {
-        packages.x86_64-linux = {
+        checks = self.packages.${system} // {
+          devshell = self.devShells.${system}.default;
+        };
+
+        packages = {
+          release-packages = releasePackages;
+
+          # LXC base OS
           lxc = base-os.config.system.build.tarball;
           lxc-metadata = base-os.config.system.build.metadata;
           lxc-import-image = lxc-import-image;
 
+          # Used by the installer
           install-extended-os = pkgs.writeShellScriptBin "install-extended-os" (builtins.readFile ./scripts/install-extended-os.sh);
 
-          dummy-system-for-cache = (self.lib.mrlinuxSystem {
+          dummy-extended-lxc = (self.lib.mrlinuxSystem {
             format = "lxc";
             system = system;
             modules = [
@@ -46,9 +77,42 @@
               }
             ];
           }).config.system.build.toplevel;
+
+          dummy-extended-orb = (self.lib.mrlinuxSystem {
+            format = "orb";
+            system = system;
+            modules = [
+              {
+                networking.hostName = "mrlinux";
+                developer = {
+                  username = "dummy";
+                  uid = 1000;
+                  gid = 1000;
+                  sshKeys = [
+                    "ssh-ed25519 AAAAC"
+                  ];
+                };
+              }
+            ];
+          }).config.system.build.toplevel;
         };
-      }
-    ) // {
+
+        devShells.default = pkgs.devshell.mkShell ({ config, ... }: {
+          commands = [
+            {
+              help = "Push release artifacts to our public attic cache";
+              name = "do-push-release";
+              command = ''
+                ${pkgs.attic-client}/bin/attic push alpha:release-public \
+                  $(nix build .#packages.x86_64-linux.release-packages --no-link --print-out-paths) \
+                  $(nix build .#packages.aarch64-linux.release-packages --no-link --print-out-paths)
+              '';
+            }
+          ];
+        });
+
+      }))
+    // {
       lib.mrlinuxSystem = { format, system, modules }:
         assert (builtins.typeOf format) == "string";
         assert (builtins.typeOf system) == "string";
